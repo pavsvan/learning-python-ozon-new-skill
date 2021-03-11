ead34b67efaa5522317379b4044090f49fd3fc6d
pavsvan <pavsvan@gmail.com>
2021-03-12 04:38:23 +0800

    "прокручиваем" цикл for

--- lesson-2/index.py
+++ lesson-2/index.py
@@ -1,2 +1,9 @@
 cars = ['volvo', 'bmw', 'audi']
-print(cars[0])
+# print(cars[0])
+
+# for car in cars:
+#    print(car)
+
+# аналогичный пример для javascript
+for i in range(0, len(cars)):
+    print(cars[i])
